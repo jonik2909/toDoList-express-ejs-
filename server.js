f5106f1cd0e1797139e475b61bedc48b2d8a3e82
Buronov <boronovjonibek@gmail.com>
2022-02-23 21:33:32 +0900

    feat: ToDoList logic was formed

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
+var  items = ["one", "two"];
 
 app.get('/', (req, res) => {
-  res.render('index', {foo: 'FOO'});
+
+
+  var today = new Date();
+
+  var options  = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("kr-KR", options);
+
+  res.render('index', {kindOfDay: day, newListItem: items});
 });
+
+
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+
+
+})
+
+
+
+
+
+
+
 
 const port = 3000
 app.listen(port, () => {
